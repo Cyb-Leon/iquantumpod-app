5c013cb2e44ed61772555fb598943e93568ba8ac
Cyb-Leon <zamokuhlendaba@gmail.com>
2026-01-04 13:39:56 +0200

    update logo theme

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,31 @@
-import { Mic } from "lucide-react"
-
+import Image from "next/image"
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <div className="w-8 h-8 bg-gradient-to-br from-amber-500 to-yellow-600 rounded-lg flex items-center justify-center">
-        <Mic className="w-5 h-5 text-black" />
+      <div className="w-28 h-28 flex items-center justify-center">
+
+        <Image
+          src="/iquantumPodcast.png"
+          alt="iQuantum Podcast"
+          width={100}
+          height={100}
+          className="object-contain"
+        />
       </div>
-      <span className="text-xl font-bold text-white">iQuantum Podcast</span>
+      <span
+        className="text-2xl font-bold bg-clip-text text-transparent"
+        style={{
+          backgroundImage: 'linear-gradient(to right,rgb(255, 0, 0),rgb(255, 11, 11),rgb(255, 106, 0),rgb(255, 191, 0),rgb(108, 162, 250),rgb(148, 1, 9),rgb(93, 7, 143))'
+        }}
+      >
+        Quantum
+      </span>
+      <span className="text-2xl font-bold bg-clip-text text-transparent"
+        style={{
+          backgroundImage: 'linear-gradient(rgb(108, 162, 250))'
+        }}>
+          Podcast
+        </span>
     </div>
   )
 }
